e9ded03178a9bdced436e63c90ce3dc350da79ef
Morty Smith <faseu81@gmail.com>
2025-09-22 16:31:31 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
-// Forçando novo deploy
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Adiciona a configuração de imagens
   images: {
     remotePatterns: [
       {
-        // Permissão para a sua logo
         protocol: 'https',
         hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
       },
       {
-        // Permissão para as capas dos filmes e séries
         protocol: 'https',
         hostname: 'image.tmdb.org',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
