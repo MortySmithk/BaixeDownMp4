6d418f6ae734f09085b9271a8ba0cbb487eb69fa
Morty Smith <faseu81@gmail.com>
2025-09-22 15:45:23 -0300

    gdg

--- app/download/movie/[id]/route.ts
+++ app/download/movie/[id]/route.ts
@@ -1,4 +1,3 @@
-// app/download/movie/[id]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
--- app/download/tv/[id]/[season]/[episode]/route.ts
+++ app/download/tv/[id]/[season]/[episode]/route.ts
@@ -1,4 +1,3 @@
-// app/download/tv/[id]/[season]/[episode]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
